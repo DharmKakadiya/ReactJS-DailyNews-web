6f4c8570fd8a145a8e213072e1b44ea88df94c71
DharmKakadiya <dharmkakadiya007@gmail.com>
2023-04-14 17:06:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ export default class App extends Component {
         <BrowserRouter>
         <Navbar/>
         <Routes>
-            <Route exact path="/" element={<News country="in" category="general"/>}/> 
-            <Route exact path="/business" element={<News country="in" category="business"/>}/> 
-            <Route exact path="/entertainment" element={<News country="in" category="entertainment"/>}/> 
-            <Route exact path="/general" element={<News country="in" category="general"/>}/> 
-            <Route exact path="/health" element={<News country="in" category="health"/>}/> 
-            <Route exact path="/science" element={<News country="in" category="science"/>}/> 
-            <Route exact path="/sports" element={<News country="in" category="sports"/>}/> 
-            <Route exact path="/technology" element={<News country="in" category="technology"/>}/> 
+            <Route exact path="/" element={<News key="general" country="in" category="general"/>}/> 
+            <Route exact path="/business" element={<News key="business" country="in" category="business"/>}/> 
+            <Route exact path="/entertainment" element={<News key="entertainment" country="in" category="entertainment"/>}/> 
+            <Route exact path="/general" element={<News key="general" country="in" category="general"/>}/> 
+            <Route exact path="/health" element={<News key="health" country="in" category="health"/>}/> 
+            <Route exact path="/science" element={<News key="science" country="in" category="science"/>}/> 
+            <Route exact path="/sports" element={<News key="sports" country="in" category="sports"/>}/> 
+            <Route exact path="/technology" element={<News key="technology" country="in" category="technology"/>}/> 
         </Routes>
         </BrowserRouter> 
         {/* </Navbar> */}
