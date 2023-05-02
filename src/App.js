d3da31dfcbcb3267b4a3f1eff3ccc7c3171f8e0c
DharmKakadiya <dharmkakadiya007@gmail.com>
2023-05-02 00:15:31 -0400

    code chnaged

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Navbar from './conponents/Navbar';
 import News from './conponents/News';
 import {BrowserRouter,Route, Routes } from 'react-router-dom';
 
+
 export default class App extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
         <Navbar/>
+        
         <Routes>
             <Route exact path="/" element={<News key="general" country="in" category="general"/>}/> 
             <Route exact path="/business" element={<News key="business" country="in" category="business"/>}/> 
